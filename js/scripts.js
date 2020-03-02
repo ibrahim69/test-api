3a083b382ba89399248d6c3e221548ec0e55fc33
Ibrahim Abdul Majid <ibrahim69@pop-os.localdomain>
2020-03-02 08:01:28 +0700

    change API

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,135 @@
+// Deklarasi
+var idArr = [];
 
-var root = document.getElementById('root');
-function createCORSRequest(method, url) {
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-  
-      // Check if the XMLHttpRequest object has a "withCredentials" property.
-      // "withCredentials" only exists on XMLHTTPRequest2 objects.
-      xhr.open(method, url, true);
-  
-    } else if (typeof XDomainRequest != "undefined") {
-  
-      // Otherwise, check if XDomainRequest.
-      // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-      xhr = new XDomainRequest();
-      xhr.open(method, url);
-  
-    } else {
-  
-      // Otherwise, CORS is not supported by the browser.
-      xhr = null;
-  
-    }
-    return xhr;
-}
+// Menampilkan data dai API ke HTML
 
-function makeCorsRequest() {
-    // This is a sample server that supports CORS.
-    var url = 'https://ghibliapi.herokuapp.com/films';
-  
-    var xhr = createCORSRequest('GET', url);
-    if (!xhr) {
-        alert('CORS not supported');
-        return;
-    }
-  
-    // Response handlers.
-    xhr.onload = function() {
-        var data = JSON.parse(this.response);
-        if (xhr.status >= 200 && xhr.status < 400) {
-            data.forEach(element => {
-              console.log(element.title)
-            })
-          } else {
-            console.log('error')
+var dataTable = $('#example').DataTable({
+    dom: 'Bfrtip',
+    processing: true,
+    serverSide: false,
+    ajax: {
+        'url' : 'http://api.wibs.sch.id/v2/meal/post/datatable.food-category',
+        'type' : 'POST',
+        'contentType' : "application/json",
+        'dataType' : 'json',
+        headers: {
+            'Application-Token' : 'geSzgVahOlowulcgHEtQmu9Ybofk1lRnPFd3V5atSEu1SD1dt2'
+        },
+    },
+    columns: [
+        {
+            data: 'id'
+        },
+        {
+            data: 'name'
+        },
+        {
+            data: 'type'
         }
+    ],
+    select: true
+});
 
-        // var text = xhr.responseText;
-        // var title = getTitle(text);
-        // alert('Response from CORS request to ' + url + ': ' + title);
+// POST New Data
+$('#modal-new-item .submit').click(function() {
+    var formData = {
+        'name' : $('.nama-sayur').val(),
+        'type' : $('.type-sayur').val()
     };
-  
-    xhr.onerror = function() {
-      alert('Woops, there was an error making the request.');
-    };
-  
-    xhr.send();
 
-    console.log(xhr);
-}
+    // process the form
+    $.ajax({
+        type : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+        url: 'http://api.wibs.sch.id/v2/meal/post/food-category.store', // the url where we want to POST
+        data : formData, // our data object,
+        dataType : 'json', // what type of data do we expect back from the server
+        headers: {
+            'Application-Token' : 'geSzgVahOlowulcgHEtQmu9Ybofk1lRnPFd3V5atSEu1SD1dt2'
+        }
+        // encode : true
+    })
+        // using the promise callback
+        .done(function(data) {
+            console.log(data);
+            alert('data sudah Masuk page ini akan di reload untuk mengambil data');
+            location.reload();
+        })
+
+        .fail(function(data) {
+            // show any errors
+            // best to remove for production
+            console.log(data);
+            alert( data + ' Data yg kirim Error')
+        })
+
+        
+    // stop the form from submitting the normal way and refreshing the page
+    event.preventDefault();
+})
+
+
+// Get class selected for Update Data
+$('#example tbody').on('click', 'tr', function() {
+
+    if( $(this).hasClass('selected') ) {
+        $(this).removeClass('selected')
+    } else {
+        dataTable.$('tr.selected').removeClass('selected');
+        $(this).addClass('selected');
+    }
+    
+    var data = dataTable.row(this).data()
+    $('.update').removeAttr('disabled');
+
+    var rows = $('tr.selected')
+    var idData = dataTable.row(rows).data();
+    $.each($(idData), function(key, value) {
+        idArr.push(value['id'])
+    })
+})
+
+// Retrieve id table to modal update
+$('.btn.update').click(function() {
+    $('.id-group').val(idArr);
+})
+
+// Update Data
+$('#modal-update-item .update').click(function() {
+
+    var formData = {
+        'id' : $('.id-group').val(),
+        'name' : $('.update-nama-sayur').val(),
+        'type' : $('.update-type-sayur').val()
+    };
+
+    console.log(formData)
+
+    // process the form
+    $.ajax({
+        type : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+        url: 'http://api.wibs.sch.id/v2/meal/post/food-category.update', // the url where we want to Update
+        data : formData, // our data object,
+        dataType : 'json', // what type of data do we expect back from the server
+        headers: {
+            'Application-Token' : 'geSzgVahOlowulcgHEtQmu9Ybofk1lRnPFd3V5atSEu1SD1dt2'
+        }
+        // encode : true
+    })
+        // using the promise callback
+        .done(function(data) {
+            console.log(data)
+            alert('data berhasil di update & page ini akan di reload');
+            location.reload();
+        })
+
+        .fail(function(data) {
+            // show any errors
+            // best to remove for production
+            console.log(data);
+            alert('Oops data yg anda masukkan gagal ' + formData)
+        })
+
+        
+    // stop the form from submitting the normal way and refreshing the page
+    event.preventDefault();
+})
